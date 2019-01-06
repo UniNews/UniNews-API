99abcbee860690bd96db5eb917199fe37cd98089
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2019-01-06 18:56:11 +0700

    [Deleted]: firebase-helper modules

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-const { firebaseHelper, firestore } = require("./../config/admin.js");
+const {  firestore } = require("./../config/admin.js");
 
-const newsCollection = firestore.collection('news');
+const newsCollection = firestore.collection("news");
 
 const {
   notFoundErrorResponse,
@@ -13,50 +13,12 @@ const {
 
 /* GET all news. */
 router.get("/", function(req, res, next) {
-  firebaseHelper.firestore
-    .backup(firestore, newsCollection)
-    .then(news => {
-      // res.status(200).send("KUY");
-      if (news.exists) {
-        successResponse(res, news);
-      } else {
-        notFoundErrorResponse(res, news);
-      }
-    })
-    .catch(err => {
-      timeOutErrorResponse(res, err);
-    });
+
 });
 
 /*Get by specific id*/
 router.get("/:id", function(req, res, next) {
-  firebaseHelper.firestore
-    .backup(firestore, newsCollection)
-    .then(news => {
-      console.log('varit')
-      let a = newsCollection.where('id','==',id).get()
-      if(a.exists){
-        successResponse(res,a)
-      }else{
-        res.status(200).send("KUY");
-        notFoundErrorResponse(res,a)
-      }
-        // snap=>{
-        //   if(!snap.empty){
-        //     successResponse(res,snap.data());
-        //     return snap.data()
-        //   }
-        //   else{
-        //     res.status(200).send("KUY");
-        //     notFoundErrorResponse(res, n);
-        //     return null
-        //   }
-        }
-      )
-    .catch(err => {
-      console.log('warat')
-      timeOutErrorResponse(res, err);
-    });
+ 
 });
 
 module.exports = router;
