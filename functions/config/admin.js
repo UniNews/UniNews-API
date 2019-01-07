71773cecf1688402bc7d43ca78f7b27271557f3f
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2019-01-07 21:35:45 +0700

    [Added]: cors to open access across domain-boundaries

--- functions/config/admin.js
+++ functions/config/admin.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-const {auth} = require('firebase-admin')
+const { auth } = require('firebase-admin')
+const cors = require('cors')({
+  origin: true
+})
+
 admin.initializeApp(functions.config().firebase)
 var firestore = admin.firestore()
-const authService = auth();
+const authService = auth()
 firestore.settings({ timestampsInSnapshots: true })
 
 module.exports = {
-  firestore,authService
+  firestore,
+  authService,
+  cors
 }
--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-const { firestore,authService } = require('./../config/admin.js')
+const { firestore, authService, cors } = require('./../config/admin.js')
 
 const newsCollection = firestore.collection('news')
 
@@ -13,64 +13,86 @@ const {
 
 /* GET all news. */
 router.get('/', function (req, res, next) {
-  var res_data = []
-  var news = newsCollection
-    .orderBy('timeStamp', 'desc')
-    .get()
-    .then(snap => {
-      snap.forEach(doc => {
-        let eachNews = doc.data()
-        eachNews['id'] = doc.id
-        res_data.push(eachNews)
-      })
-      successResponse(res, res_data)
+  cors(
+    (req,
+    res,
+    () => {
+      var res_data = []
+      var news = newsCollection
+        .orderBy('timeStamp', 'desc')
+        .get()
+        .then(snap => {
+          snap.forEach(doc => {
+            let eachNews = doc.data()
+            eachNews['id'] = doc.id
+            res_data.push(eachNews)
+          })
+          successResponse(res, res_data)
+        })
+        .catch(err => {
+          timeOutErrorResponse(res, err)
+        })
     })
-    .catch(err => {
-      timeOutErrorResponse(res, err)
-    })
+  )
 })
 
 /* Get by specific id */
 router.get('/:id', function (req, res, next) {
-  let id = req.params.id
-  var news = newsCollection
-    .doc(id)
-    .get()
-    .then(snap => {
-      if (snap.exists) {
-        successResponse(res, snap.data())
-      } else {
-        notFoundErrorResponse(res)
-      }
-    })
-    .catch(err => {
-      timeOutErrorResponse(res, err)
-    })
-})
-
-router.post('/:id/comments',async(req,res,next)=>{
-    let user_token = req.body.user_token
-    let msg = req.body.msg
-    let newsId = req.params.id
-    let decodedIdToken;
-        try {
-            var res_data={}
-            var getDoc = newsCollection.doc(newsId).collection('comments').get()
-            decodedIdToken = await authService.verifyIdToken(user_token).then(snap=>{
-            if(snap.exists){
-            res_data['id']= snap.id
-            res_data['name']= snap.name
-            res_data['msg']= msg
-            getDoc.push(res_data)
-            successResponse(res,res_data)
-            }
-            else{
+  cors(
+    (req,
+    res,
+    () => {
+      let id = req.params.id
+      var news = newsCollection
+        .doc(id)
+        .get()
+        .then(snap => {
+          if (snap.exists) {
+            successResponse(res, snap.data())
+          } else {
             notFoundErrorResponse(res)
-            }
+          }
         })
+        .catch(err => {
+          timeOutErrorResponse(res, err)
+        })
+    })
+  )
+
+  router.post('/:id/comments', async (req, res, next) => {
+    cors(
+      (req,
+      res,
+      () => {
+        let user_token = req.body.user_token
+        let msg = req.body.msg
+        let newsId = req.params.id
+        let decodedIdToken
+        try {
+          var res_data = {}
+          var getDoc = newsCollection
+            .doc(newsId)
+            .collection('comments')
+            .get()
+          decodedIdToken = await authService
+            .verifyIdToken(user_token)
+            .then(snap => {
+              if (snap.exists) {
+                res_data['id'] = snap.id
+                res_data['name'] = snap.name
+                res_data['msg'] = msg
+                getDoc.push(res_data)
+                successResponse(res, res_data)
+              } else {
+                notFoundErrorResponse(res)
+              }
+            })
         } catch (error) {
-            timeOutErrorResponse(res, error)
+          timeOutErrorResponse(res, error)
         }
+      })
+    )
+  })
 })
 
 module.exports = router
