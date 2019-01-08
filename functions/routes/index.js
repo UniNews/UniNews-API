618b02d3802e43aabb472ac59ab89e33c6ae4f4e
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2019-01-08 17:49:15 +0700

    Fixed post rating function

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -68,8 +68,8 @@ router.post('/:id/comments', function (req, res, next) {
     authService
       .verifyIdToken(user_token)
       .then(function (decodedToken) {
-        res_data['id'] = "sss"
-        res_data['name'] = "ss"
+        res_data['user_id'] = decodedToken.user_id
+        res_data['name'] = decodedToken.name
         res_data['msg'] = msg
         newsCollection
         .doc(newsId).update(
@@ -78,6 +78,7 @@ router.post('/:id/comments', function (req, res, next) {
           }
         )
         successResponse(res, 'Post comment successfully.', res_data)
+        return decodedToken.user_id
       })
       .catch(function (error) {
         notFoundErrorResponse(res, 'Token is either expired or not valid.')
@@ -92,17 +93,20 @@ router.post('/:id/rating', function (req, res, next) {
     let newsId = req.params.id
     var res_data = {}
     authService
-      .verifyIdToken(user_token)
-      .then(function (decodedToken) {
-        res_data['id'] = decodedToken.id
-        res_data['name'] = decodedToken.name
-        res_data['rating'] = rating
-        newsCollection
-        .doc(newsId)
-        .collection('rating')
-        .add(res_data)
-        successResponse(res, 'Post rating successfully.', res_data)
-      })
+    .verifyIdToken(user_token)
+    .then(function (decodedToken) {
+      res_data['user_id'] = decodedToken.user_id
+      res_data['name'] = decodedToken.name
+      res_data['rating'] = rating
+      newsCollection
+      .doc(newsId).update(
+        {
+          'rating': admin.firestore.FieldValue.arrayUnion(res_data)
+        }
+      )
+      successResponse(res, 'Post rating successfully.', res_data)
+      return decodedToken.user_id
+    })
       .catch(function (error) {
         notFoundErrorResponse(res, 'Token is either expired or not valid.')
       })
